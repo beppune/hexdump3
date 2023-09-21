ca477b1e84bcbbdd21e4d076799a1923847233aa
beppe <manzo.giuseppe@gmail.com>
2023-09-21 22:34:45 +0200

    comments to DumpChar

--- procs.asm
+++ procs.asm
@@ -3,22 +3,28 @@ global DumpChar
 
 section .text
 
+	; DumpChar:		Put an Hex representation of byte into given position
+	; UPDATED:		21/09/2023
+	; INPUT:		AL:		byte to be represented
+	;				EDI:	base position of position into representation string
+	; OUTPUT:		None
+	; MODIFIES:		bytes at [edi + 2] and [edi + 1]
 	DumpChar:
-		push ebx
+		push ebx				; save ebx value
 		
-		xor ebx, ebx
+		xor ebx, ebx			; zero ebx
 
-		mov bl, al
-		and bl, 0Fh
-		mov bl, [HexStr + ebx]
-		mov [edi + 2], bl
+		mov bl, al				; copy given byte to al
+		and bl, 0Fh				; and byte to get Least Significant Nybble
+		mov bl, [HexStr + ebx]	; move matching byte of bl value into bl
+		mov [edi + 2], bl		; move LSN char into string position
 
-		mov bl, al
-		shr bl, 4
-		mov bl, [HexStr + ebx]
-		mov [edi + 1], bl
+		mov bl, al				; copy given byte into al
+		shr bl, 4				; shift bl 4 btes to right to get Most Significant Nybble
+		mov bl, [HexStr + ebx]	; move matching byte of bl value into bl
+		mov [edi + 1], bl		; move MSN char into string position
 
-		pop ebx
+		pop ebx					; restore ebx
 		
-		ret
+		ret						; return from procedire
 		HexStr: db "0123456789ABCDEF"
